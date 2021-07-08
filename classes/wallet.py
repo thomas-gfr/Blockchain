84297b9ce66552904dc7048a5c8de53101aa96ee
thomas-gfr <g.thomas.pro806@gmail.com>
2021-07-08 11:11:46 +0200

    generation unique id for wallet

--- classes/wallet.py
+++ classes/wallet.py
@@ -1,2 +1,10 @@
+import uuid
+
+
 class Wallet:
     unique_id = ''
+
+    def generate_unique_id(self):
+        self.unique_id = uuid.uuid4()
+
+    # Ajouter verifications pour les autres wallet
---
+++ main.py
@@ -0,0 +1,11 @@
+from classes.wallet import Wallet
+
+
+def main():
+    print("Hello World!")
+    wallet = Wallet()
+    print(wallet.generate_unique_id())
+
+
+if __name__ == "__main__":
+    main()
