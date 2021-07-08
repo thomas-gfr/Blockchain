242d1c46ceb40d3826894237440479049638be88
thomas-gfr <g.thomas.pro806@gmail.com>
2021-07-08 11:17:53 +0200

    add informatiosn for wallet

--- classes/wallet.py
+++ classes/wallet.py
@@ -3,8 +3,19 @@ import uuid
 
 class Wallet:
     unique_id = ''
+    balance = 0
+    history = []
 
     def generate_unique_id(self):
         self.unique_id = uuid.uuid4()
 
     # Ajouter verifications pour les autres wallet
+
+    def add_balance(self, value):
+        self.balance += value
+
+    def sub_balance(self, value):
+        self.balance -= value
+
+    def send(self):
+        pass
